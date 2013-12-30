8c1bbc880a205ca6ad4ecbc3cd1b5bd60ac55db4
Jon Lesser <jonlesser@gmail.com>
2013-12-30 09:20:51 +0000

    first pass at models

--- controllers/hunt_list.py
+++ controllers/hunt_list.py
@@ -1,13 +1,24 @@
 import jinja2
 import os
+import logging
 import webapp2
+
+from google.appengine.api import users
+from models.models import Hunts
+from models.models import Hunters
 
 templates = jinja2.Environment(loader=jinja2.FileSystemLoader('templates'))
 
 class HuntListHandler(webapp2.RequestHandler):
   def get(self):
+    # Get a list of hunts associated with this user.
+    user = users.get_current_user()
+
     template = templates.get_template('hunt_list.html')
     self.response.headers.add('X-UA-Compatible', 'IE=edge')
     self.response.out.write(template.render({
       'dev': os.environ["SERVER_SOFTWARE"].startswith('Development'),
     }))
+
+  def post(self):
+    pass
---
+++ models/models.py
@@ -0,0 +1,24 @@
+from google.appengine.ext import ndb
+
+class Hunters(ndb.Model):
+  created = ndb.DateTimeProperty(auto_now_add=True)
+  hunts = ndb.KeyProperty(kind='Hunts', repeated=True)
+  modified = ndb.DateTimeProperty(auto_now=True)
+  user = ndb.UserProperty(auto_current_user_add=True)
+
+class Hunts(ndb.Model):
+  created = ndb.DateTimeProperty(auto_now_add=True)
+  creator = ndb.UserProperty(auto_current_user_add=True)
+  hurl = ndb.StringProperty(required=True)
+  modified = ndb.DateTimeProperty(auto_now=True)
+  modifier = ndb.UserProperty(auto_current_user=True)
+  name = ndb.StringProperty(required=True)
+
+class Puzzles(ndb.Model):
+  created = ndb.DateTimeProperty(auto_now_add=True)
+  creator = ndb.UserProperty(auto_current_user_add=True)
+  doc_id = ndb.StringProperty(repeated=True)
+  modified = ndb.DateTimeProperty(auto_now=True)
+  modifier = ndb.UserProperty(auto_current_user=True)
+  name = ndb.StringProperty(required=True)
+  url = ndb.StringProperty()
